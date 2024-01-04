7fc2f5f7e3095384d5b783e6499de324d8943b62
Pedro0974 <pedro0702lucas@gmail.com>
2024-01-04 14:37:18 -0300

    create template frontend with expo

---
+++ frontend/src/constants/colors.js
@@ -0,0 +1,11 @@
+// src/constants/colors.js
+
+export const COLORS = {
+    darkblue: '#001F3', // cor principal
+    lightblue: '#0074E4', // cor secundaria
+    darkgrey: '#333333', // texto
+    lightgray: '#F2F2F2', // fundo
+    green: '#28A745', // indicador de ação
+    yellow: '#FFC107', // indicador de ação
+    white: '#FFFFFF' // texto
+}
